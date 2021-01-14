c8820a6afde3c0ce8f6517454b90afa596ab11b4
Archy404 <76533300+Archy404@users.noreply.github.com>
2021-01-14 16:57:10 +0100

    Added Tables for Different Classes to Database and Wrote initializers. Appointment Specialization SpecializationEnum

--- src/Databaseconnection.java
+++ src/Databaseconnection.java
@@ -26,113 +26,6 @@ public class Databaseconnection {
         return statement.executeQuery("SELECT * FROM User WHERE emailAddress ='" + email + "';");
     }
 
-    public void connect() throws SQLException, ClassNotFoundException {
-            Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:eHealthcareUsers.db");
-            initialise();
-    }
-
-    public void initialise() throws SQLException {
-        if(!hasData){
-            hasData = true;
-            Statement state = connection.createStatement();
-            ResultSet res = state.executeQuery("SELECT name FROM sqlite_master WHERE type ='table' AND name='User'");
-
-            if (!res.next()) {
-                System.out.println("Building database...");
-                buildUsertable();
-                buildSpecializationEnumtable();
-                buildSpecializationtable();
-                buildAppointmenttable();
-                System.out.println("Database build.");
-
-            }
-
-        }
-    }
-
-    private void buildAppointmenttable() throws SQLException{
-        System.out.println("Building Appointment table...");
-
-        Statement state = connection.createStatement();
-        state.execute( "CREATE TABLE Appointment (" +
-                "AppointmentID int," +
-                "PatientID int," +
-                "PhysicianID int," +
-                "TimeAndDate text," +
-                "TransferorderID int," +
-                "FOREIGN KEY (PatientID) REFERENCES User (UserID)," +
-                "FOREIGN KEY (PhysicianID) REFERENCES User (UserID)" +
-                " )");
-
-        System.out.println("complete.");
-    }
-
-    private void buildSpecializationtable() throws SQLException {
-        System.out.println("Building Specialization table...");
-
-        Statement state = connection.createStatement();
-        state.execute( "CREATE TABLE Specialization (" +
-                "UserID int," +
-                "Specialization VARCHAR(255)," +
-                "PRIMARY KEY (UserID, Specialization)," +
-                "FOREIGN KEY (UserID) REFERENCES User (UserID)" +
-                ")");
-
-        System.out.println("complete.");
-    }
-
-    private void buildSpecializationEnumtable() throws SQLException{
-        System.out.println("Building SpecializationEnum table...");
-
-        Statement state = connection.createStatement();
-        state.execute( "CREATE TABLE Specialization_Enum (" +
-                "Specialization_EnumID int IDENTITY(1,1) PRIMARY KEY," +
-                "Specialization VARCHAR(255) NOT NULL UNIQUE)");
-
-        System.out.println("complete.");
-    }
-
-    private void buildUsertable() throws SQLException{
-        System.out.println("Building User table...");
-        Statement state = connection.createStatement();
-        state.execute( "CREATE TABLE User (" +
-                "emailAddress VARCHAR(255) NOT NULL," +
-                "password VARCHAR(255) NOT NULL," +
-                "firstName VARCHAR(255) NOT NULL," +
-                "lastName VARCHAR (255) NOT NULL," +
-                "city VARCHAR(255) NOT NULL," +
-                "street VARCHAR(255) NOT NULL," +
-                "houseNumber VARCHAR(255) NOT NULL," +
-                "postalCode VARCHAR(5)," +
-                "phoneNumber VARCHAR(20) NOT NULL," +
-                "title VARCHAR (255)," +
-                "isPhysician BOOLEAN DEFAULT FALSE," +
-                "isAdmin BOOLEAN DEFAULT FALSE," +
-                "UserID int IDENTITY(1,1) PRIMARY KEY)");
-
-        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO User (emailAddress , password," +
-                "firstName , lastName," +
-                "city , street, houseNumber, postalCode," +
-                "phoneNumber, title, isPhysician, isAdmin) VALUES (?,?,?,?,?,?,?,?,?,?,?,?);");
-
-        preparedStatement.setString(1, "asd");
-        preparedStatement.setString(2, User.hashPassword("asd"));
-        preparedStatement.setString(3, "Achim");
-        preparedStatement.setString(4, "Glaesmann");
-        preparedStatement.setString(5, "Frankfurt");
-        preparedStatement.setString(6, "Ben-Gurion-Ring");
-        preparedStatement.setString(7, "48C");
-        preparedStatement.setString(8, "60437");
-        preparedStatement.setString(9, "015738340183");
-        preparedStatement.setString(10,"hobo");
-        preparedStatement.setBoolean(11, false);
-        preparedStatement.setBoolean(12,true);
-        preparedStatement.execute();
-
-        System.out.println("complete.");
-    }
-
     public void addUser(Patient patient) throws SQLException, ClassNotFoundException {
         if(connection == null){
             connect();
@@ -192,6 +85,130 @@ public class Databaseconnection {
         preparedStatement.setBoolean(12, true);
         preparedStatement.execute();
     }
+
+    private void connect() throws SQLException, ClassNotFoundException {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:eHealthcareUsers.db");
+            initialise();
+    }
+
+    private void initialise() throws SQLException {
+        if(!hasData){
+            hasData = true;
+            Statement state = connection.createStatement();
+            ResultSet res = state.executeQuery("SELECT name FROM sqlite_master WHERE type ='table' AND name='User'");
+
+            if (!res.next()) {
+                System.out.println("Building database...");
+                buildUserTable();
+                buildSpecializationEnumTable();
+                buildSpecializationTable();
+                buildAppointmentTable();
+                System.out.println("Database build.");
+
+            }
+
+        }
+    }
+
+    private void buildAppointmentTable() throws SQLException{
+        System.out.println("Building Appointment table...");
+
+        Statement state = connection.createStatement();
+        state.execute( "CREATE TABLE Appointment (" +
+                "AppointmentID int," +
+                "PatientID int," +
+                "PhysicianID int," +
+                "TimeAndDate text," +
+                "TransferorderID int," +
+                "FOREIGN KEY (PatientID) REFERENCES User (UserID)," +
+                "FOREIGN KEY (PhysicianID) REFERENCES User (UserID)" +
+                " )");
+
+        System.out.println("complete.");
+    }
+
+    private void buildSpecializationTable() throws SQLException {
+        System.out.println("Building Specialization table...");
+
+        Statement state = connection.createStatement();
+        state.execute( "CREATE TABLE Specialization (" +
+                "UserID int," +
+                "Specialization VARCHAR(255)," +
+                "PRIMARY KEY (UserID, Specialization)," +
+                "FOREIGN KEY (UserID) REFERENCES User (UserID)" +
+                ")");
+
+        System.out.println("complete.");
+    }
+
+    private void buildSpecializationEnumTable() throws SQLException{
+        System.out.println("Building SpecializationEnum table...");
+
+        Statement state = connection.createStatement();
+        state.execute( "CREATE TABLE Specialization_Enum (" +
+                "Specialization_EnumID int IDENTITY(1,1) PRIMARY KEY," +
+                "Specialization VARCHAR(255) NOT NULL UNIQUE)");
+
+        System.out.println("complete.");
+    }
+
+    private void buildUserTable() throws SQLException{
+        System.out.println("Building User table...");
+        Statement state = connection.createStatement();
+        state.execute( "CREATE TABLE User (" +
+                "emailAddress VARCHAR(255) NOT NULL," +
+                "password VARCHAR(255) NOT NULL," +
+                "firstName VARCHAR(255) NOT NULL," +
+                "lastName VARCHAR (255) NOT NULL," +
+                "city VARCHAR(255) NOT NULL," +
+                "street VARCHAR(255) NOT NULL," +
+                "houseNumber VARCHAR(255) NOT NULL," +
+                "postalCode VARCHAR(5)," +
+                "phoneNumber VARCHAR(20) NOT NULL," +
+                "title VARCHAR (255)," +
+                "isPatient BOOLEAN DEFAULT FALSE," +
+                "isPhysician BOOLEAN DEFAULT FALSE," +
+                "isAdmin BOOLEAN DEFAULT FALSE," +
+                "UserID int IDENTITY(1,1) PRIMARY KEY)");
+
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO User (" +
+                "emailAddress," +
+                "password," +
+                "firstName," +
+                "lastName," +
+                "city," +
+                "street," +
+                "houseNumber," +
+                "postalCode," +
+                "phoneNumber," +
+                "title," +
+                "isPatient," +
+                "isPhysician," +
+                "isAdmin) " +
+                "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?);");
+
+        preparedStatement.setString(1, "asd");
+        preparedStatement.setString(2, User.hashPassword("asd"));
+        preparedStatement.setString(3, "Achim");
+        preparedStatement.setString(4, "Glaesmann");
+        preparedStatement.setString(5, "Frankfurt");
+        preparedStatement.setString(6, "Ben-Gurion-Ring");
+        preparedStatement.setString(7, "48C");
+        preparedStatement.setString(8, "60437");
+        preparedStatement.setString(9, "015738340183");
+        preparedStatement.setString(10,"hobo");
+        preparedStatement.setBoolean(11, false);
+        preparedStatement.setBoolean(12,false);
+        preparedStatement.setBoolean(13,true);
+        preparedStatement.execute();
+
+        System.out.println("complete.");
+    }
+
+
+
+
 
 
 }
